7406c523b31e187f5098056f6858e191679be447
Dharmesh <dharmeshgurnani2000@gmail.com>
2022-08-23 01:05:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ChakraProvider} from "@chakra-ui/react"
 import theme from "./Style/theme"
 import Header from "./components/Header"
 import { Main } from "./components/Main"
-import Footer from "./components/Footer"
+//import Footer from "./components/Footer"
 
 export const App = () => (
   <ChakraProvider theme={theme} maxH={'100vh'}>
